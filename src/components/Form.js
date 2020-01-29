cd91d62263f48a784becbf00a295ef963ee36269
AntonioSE <antonio.salazar@oliverwyman.com>
2020-01-29 15:31:13 -0600

    working on the state for the expense

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,30 @@
 import React, {useState} from 'react';
 
+
 const Form = () => {
+
+    const [ nameExpense, saveNameExpense ] = useState('');
+    const [ amountExpense, saveAmountExpense ] = useState(0);
+
+    //When the user adds an expense
+    const addExpense = e => {
+        e.preventDefault();
+
+
+        //validate
+
+        //build the expense
+
+        //take the expense to the main component
+
+        //reset the form
+
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={addExpense}
+        >
             <h2>Add the things in which you spend your money in here</h2>
             <div className="field">
                 <label>Name of the expense</label>
@@ -10,6 +32,8 @@ const Form = () => {
                     type="text"
                     className="u-full-width"
                     placeholder='i.e. Meals'
+                    value={nameExpense}
+                    onChange={e => saveNameExpense(e.target.value)}
                 />
             </div>
 
@@ -19,6 +43,8 @@ const Form = () => {
                     type="number"
                     className="u-full-width"
                     placeholder='i.e. $200'
+                    value={amountExpense}
+                    onChange={e => saveAmountExpense( parseInt(e.target.value, 10))}
                 />
             </div>
 
